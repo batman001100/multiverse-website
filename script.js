516393125e66d2917b9567c27131641340490781
batman001100 <p65738042@gmail.com>
2026-05-24 15:29:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ function openUniverse(choice) {
     let text = "";
 
     if (choice === 1) {
-        text = "We stayed just friends... and something always felt missing.";
+        text = "We stayed just friends... safe, but something always felt missing.";
     }
     else if (choice === 2) {
-        text = "We never met... and that universe feels empty.";
+        text = "We never met... and that universe feels strangely empty.";
     }
-    else if (choice === 3) {
-        text = "Out of all universes... this one feels the most special 💫";
+    else {
+        text = "Out of all universes... this one feels the most real 💫";
     }
 
     typeText(text);
+
+    setTimeout(() => {
+        document.getElementById("final").innerHTML =
+            "✨ Maybe some universes are not chosen... they are felt.";
+    }, 4000);
 }
 
 function typeText(text) {
@@ -29,5 +34,5 @@ function typeText(text) {
             clearInterval(interval);
         }
 
-    }, 50);
+    }, 40);
 }
